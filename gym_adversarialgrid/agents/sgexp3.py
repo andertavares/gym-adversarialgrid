9e792cc453b8fda56de8efb3025628607a219983
anderson <andersonrochatavares@gmail.com>
2017-08-23 14:47:45 -0300

    improving implementation (as per Auer et al 2002)

--- gym_adversarialgrid/agents/sgexp3.py
+++ gym_adversarialgrid/agents/sgexp3.py
@@ -1,5 +1,7 @@
 import gym_adversarialgrid.agents.tabular as tabular
+from collections import defaultdict
 import random
+import math
 
 
 def categorical_draw(probabilities):
@@ -17,68 +19,79 @@ def categorical_draw(probabilities):
             return choice
 
     print('Warning: categorical_draw reached its end')
-    return choice  # I think code should not reach here
+    return len(probabilities) - 1  # I think code should not reach here
 
 
 class SGExp3(tabular.TabularQAgent):
+    """
+    Extends Exp3 (Auer et. al 1995) with the notion of state.
+    The implementation of Exp3 we are extending is the one shown in Auer et. al 2002.
+
+    References:
+
+    Auer, P., Cesa-Bianchi, N., Freund, Y., & Schapire, R. E. (1995).
+    Gambling in a rigged casino: The adversarial multi-armed bandit problem.
+    Proceedings of IEEE 36th Annual Foundations of Computer Science, 322–331.
+    https://doi.org/10.1109/SFCS.1995.492488
+
+    Auer, P., Cesa-Bianchi, N., Freund, Y., & Schapire, R. E. (2002).
+    The nonstochastic multiarmed bandit problem.
+    Society for Industrial and Applied Mathematics, 32(1), 48–77.
+    """
+
     def __init__(self, *args, **kwargs):
         super(SGExp3, self).__init__(*args, **kwargs)
 
         self.gamma = kwargs['gamma'] if 'gamma' in kwargs else 0.2
-        self.alpha = kwargs['exp3_alpha'] if 'exp3_alpha' in kwargs else 0.5
+        self.discount = kwargs['discount'] if 'discount' in kwargs else 0.9
+
+        self.policy = defaultdict(lambda: [0] * self.action_space.n)
+
+    def calculate_policy(self, state):
+        """
+        Calculates the policy for a given state and returns it
+        :param state: 
+        :return: list(float) the policy (probability vector) for that state
+        """
+        # short aliases
+        s = state  # s stands for state
+        g = self.gamma  # g stands for gamma
+        n = self.action_space.n  # n stands for the number of actions
+        pi_s = self.policy[state]  # pi_s stands for the policy in state s
+
+        sum_weights = sum(self.q[s])
+
+        # the policy is a probability vector, giving the probability of each action
+        # pi(s, . ) = [(1 - gamma)*q(s,a) + gamma / n] - for each action
+        pi_s = [((1 - g) * value / sum_weights) + (g / n) for a, value in enumerate(self.q[s])]
+
+        return pi_s
 
     def act(self, observation):
-        # performs selection
-        # total_weight = sum(self.weights.values())
-        total_prob_factor = sum(self.prob_factor(w) for w in self.weights.values())
-
-        # the number of options in this state
-        n_arms = len(self.action_space)
-
-        # initializes the list of probabilities
-        probs = []
-
-        weights = self.q[observation]
-
-        for arm in range(len(self.action_space)):
-            # probs[arm] = (1 - self.gamma) * (self.weights[arm] / total_weight)
-            # probs[arm] += self.gamma * (1.0 / float(n_arms))
-            pre_prob = self.prob_factor(weights[arm]) / total_prob_factor
-
-            # mixes pre_prob with an uniform distribution
-            # gamma is the 'fraction' of uniform that gets mixed in
-            probs.append((1.0 - self.gamma) * pre_prob + self.gamma / float(n_arms))
-
-        return categorical_draw(probs)
+        prob_vector = self.calculate_policy(observation)
+        return categorical_draw(prob_vector)
 
     def learn(self, s, a, reward, sprime, done):
-        # SO FAR IT IS THE PLAIN EXP3 UPDATE RULE
-        n_arms = len(self.action_space)
-        weights = self.q[s]
+        # aliases:
+        pi_sp = self.policy[sprime]  # the policy for the next state
+        q_sp = self.q[sprime]  # the action values for next state
+        n = self.action_space.n  # the number of actions
 
-        # total_weight = sum(self.weights.values())
-        total_prob_factor = sum(self.prob_factor(w) for w in weights.values())
+        # value of next state, it is zero if current state is terminal
+        future = sum([pi_sp[ap] * value for ap, value in enumerate(q_sp)]) if not done else 0
 
-        # rescales reward to [0,1], the original is either -1 or 1
-        scaled_rwd = (reward + 1) / 2
+        # x is a value to be scaled and weighted by its probability
+        x = reward + self.discount * future
 
-        pre_prob = self.prob_factor(weights[a]) / total_prob_factor
+        # scales x to [0, 1] - assuming minimum reward is -1 and max reward is +1
+        # rescaling as per https://en.wikipedia.org/wiki/Feature_scaling#Rescaling
+        max_x = 1 + self.discount
+        min_x = -1 - self.discount
 
-        prob_of_chosen_arm = (1 - self.gamma) * pre_prob + self.gamma / n_arms
-        # prob_of_chosen_arm = (1 - self.gamma) * (self.weights[chosen_arm] / total_weight) \
-        #                     + self.gamma * (1.0 / float(n_arms))
+        scaled_x = (x - min_x) / (max_x - min_x)
 
-        # x = reward / prob_of_chosen_arm  # probs[chosen_arm]
-        mixed_rwd = (self.gamma / n_arms) * (reward / prob_of_chosen_arm)  # probs[chosen_arm]
+        # weights the value by its probability
+        x_hat = scaled_x / self.policy[s][a]
 
-        # growth_factor = math.exp((self.gamma / n_arms) * x)
-        # self.weights[chosen_arm] *= growth_factor
-        weights[a] += mixed_rwd
-
-    def prob_factor(self, weight):
-        """
-        Uses the probability factor formula of Exp3
-        :param weight:
-        :return:
-        """
-        return (1 + self.alpha) ** weight
+        # finally updates the value
+        self.q[s][a] = self.q[s][a] * math.exp(self.gamma * x_hat / n)
