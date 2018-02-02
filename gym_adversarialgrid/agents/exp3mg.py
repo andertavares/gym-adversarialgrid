857607b11a5622fd666f0d48d79ea0e70c58b03a
anderson <andersonrochatavares@gmail.com>
2018-02-02 14:35:35 -0200

    consolidating Exp3MG_1995

--- gym_adversarialgrid/agents/exp3mg.py
+++ gym_adversarialgrid/agents/exp3mg.py
@@ -115,3 +115,76 @@ class Exp3MG(tabular.TabularQAgent):
             # print(policy)
 
         return policy
+
+
+class Exp3MG_1995(Exp3MG):
+    """
+    Extends the original Exp3 (Auer et. al 1995) with the notion of state.
+
+    References:
+
+    Auer, P., Cesa-Bianchi, N., Freund, Y., & Schapire, R. E. (1995).
+    Gambling in a rigged casino: The adversarial multi-armed bandit problem.
+    Proceedings of IEEE 36th Annual Foundations of Computer Science, 322–331.
+    https://doi.org/10.1109/SFCS.1995.492488
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(Exp3MG_1995, self).__init__(*args, **kwargs)
+
+        self.alpha = kwargs['alpha'] if 'alpha' in kwargs else 0.2
+
+    def calculate_policy(self, state):
+        """
+        Calculates the policy for a given state and returns it
+        :param state:
+        :return: list(float) the policy (probability vector) for that state
+        """
+        # short aliases
+        s = state  # s stands for state
+        g = self.gamma  # g stands for gamma
+        n = self.action_space.n  # n stands for the number of actions
+        a = self.alpha
+        pi_s = self.policy[state]  # pi_s stands for the policy in state s
+        weights = self.weights[state]
+        # print(weights)
+
+
+        # obtains the probability vector from Hedge: p_i(t) = (1+alpha)^s_i(t) / sum_{j \in K} (1+alpha)^s_j(t)
+        sum_weights_exponentials = sum([(1 + a) ** w for w in weights])
+        pre_prob = [(((1 + a) ** w) / sum_weights_exponentials) for w in weights]
+
+        # the policy is a probability vector, giving the probability of each action
+        pi_s = [((1 - g) * p) + (g / n) for p in pre_prob]
+
+        return pi_s
+
+    def learn(self, s, a, reward, sprime, done):
+        # aliases:
+        pi_sp = self.calculate_policy(sprime)
+        q = self.q  # alias for the action value function
+        n = self.action_space.n  # the number of actions
+
+        # estimation of the expected value of s' -- it is zero if current state is terminal
+        future = sum([pi_sp[aprime] * value for aprime, value in enumerate(q[sprime])]) if not done else 0
+
+        # minimax-Q-like update:
+        q[s][a] = q[s][a] + self.lrn_rate * (reward + self.discount * future - q[s][a])
+
+        x = q[s][a]
+
+        # scales x to [0, 1] - assuming minimum reward is -1 and max reward is +1
+        # rescaling as per https://en.wikipedia.org/wiki/Feature_scaling#Rescaling
+        max_x = 1  # + self.discount
+        min_x = -1  # - self.discount
+        scaled_x = (x - min_x) / (max_x - min_x)
+
+        if not 0 <= scaled_x <= 1:
+            print("WARNING! scaled_x=%f out of bounds!" % scaled_x)
+
+        # weights the value by its probability -- estimates the reward...
+        x_hat = (self.gamma / n) * (scaled_x / self.policy[s][a])
+
+        # finally updates the weight
+        # print('q(s,a), r, f, x, ~x, ^x = %.3f, %3f, %.3f, %.3f, %.3f, %.3f' % (self.q[s][a], future, reward, x, scaled_x, x_hat))
+        self.weights[s][a] += x_hat
