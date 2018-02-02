f9e6d420d00bbbd042a65a4a637d09033b6d4f7c
anderson <andersonrochatavares@gmail.com>
2018-02-02 14:48:01 -0200

    consolidating HedgeMG_1995, although it seemed to perform worse than in .ipynb

--- gym_adversarialgrid/agents/hedgemg.py
+++ gym_adversarialgrid/agents/hedgemg.py
@@ -61,3 +61,51 @@ class HedgeMG(exp3mg.Exp3MG):
             # print('q(s,a), r, f, x, ~x = %.3f, %3f, %.3f, %.3f, %.3f' % (self.q[s][a], future, reward, x, scaled_x))
             self.weights[s][action] *= math.exp(self.gamma * scaled_x / n)
             # print(self.weights[s])
+
+
+class HedgeMG_1995(HedgeMG):
+    """
+    Extends the original Hedge (Freund&Schapiro 1995)  with the notion of state.
+
+    Reference:
+    Freund, Y., & Schapire, R. E. (1995). A desicion-theoretic generalization of
+    on-line learning and an application to boosting, 55(1), 23–37.
+    https://doi.org/10.1007/3-540-59119-2_166
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(HedgeMG_1995, self).__init__(*args, **kwargs)
+
+        self.alpha = kwargs['alpha'] if 'alpha' in kwargs else 0.2
+        self.weights = defaultdict(
+            lambda: [1.0 / self.action_space.n] * self.action_space.n
+        )
+
+    def learn(self, s, a, reward, sprime, done):
+        # aliases:
+        pi_sp = self.calculate_policy(sprime)
+        q = self.q  # alias for the action value function
+        n = self.action_space.n  # the number of actions
+
+        # estimation of the expected value of s' -- it is zero if current state is terminal
+        future = sum([pi_sp[aprime] * value for aprime, value in enumerate(q[sprime])]) if not done else 0
+
+        # minimax-Q-like update:
+        q[s][a] = q[s][a] + self.lrn_rate * (reward + self.discount * future - q[s][a])
+
+        for action in range(self.action_space.n):
+            x = q[s][action]
+
+            # scales x to [0, 1] - assuming minimum reward is -1 and max reward is +1
+            # rescaling as per https://en.wikipedia.org/wiki/Feature_scaling#Rescaling
+            max_x = 1  # + self.discount
+            min_x = -1  # - self.discount
+            scaled_x = (x - min_x) / (max_x - min_x)
+
+            if not 0 <= scaled_x <= 1:
+                print("WARNING! scaled_x=%f out of bounds!" % scaled_x)
+
+            # finally updates the weight
+            # print('q(s,a), r, f, x, ~x = %.3f, %3f, %.3f, %.3f, %.3f' % (self.q[s][a], future, reward, x, scaled_x))
+            self.weights[s][action] += scaled_x
+            # print(self.weights[s])
