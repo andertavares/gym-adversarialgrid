a68e4f569ea5688e52ac07f50acc9c95a48b0e5f
anderson <andersonrochatavares@gmail.com>
2018-02-08 16:55:11 -0200

    random and fixed now extend LearningAgent
    they don't learn, but this is for compatibility

--- gym_adversarialgrid/agents/adversary.py
+++ gym_adversarialgrid/agents/adversary.py
@@ -1,14 +1,14 @@
-from gym_adversarialgrid.agents.agent import Agent
+from gym_adversarialgrid.agents.agent import LearningAgent
 
 
-class Random(Agent):
+class Random(LearningAgent):
     """
     Has a random uniform policy
 
     """
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(Random, self).__init__(*args, **kwargs)
 
     def act(self, observation):
         """
@@ -18,8 +18,20 @@ class Random(Agent):
         """
         return self.action_space.sample()
 
+    def learn(self, s, a, reward, sprime, done):
+        """
+        This method does nothing. This agent does not learn.
+        :param s:
+        :param a:
+        :param reward:
+        :param sprime:
+        :param done:
+        :return:
+        """
+        pass
 
-class Fixed(Agent):
+
+class Fixed(LearningAgent):
     """
     An agent that always return the same action
     """
@@ -29,8 +41,20 @@ class Fixed(Agent):
         Always select a fixed action
         :param action:
         """
-        super().__init__(*args, **kwargs)
+        super(Fixed, self).__init__(*args, **kwargs)
         self.action = kwargs['action']
 
     def act(self, observation):
         return self.action
+
+    def learn(self, s, a, reward, sprime, done):
+        """
+        This method does nothing. This agent does not learn.
+        :param s:
+        :param a:
+        :param reward:
+        :param sprime:
+        :param done:
+        :return:
+        """
+        pass
